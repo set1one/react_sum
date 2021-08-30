7a6401d7fb6325f8a285ec5a0e05e2af432de9db
Eugene Gnedenko <deadup4@gmail.com>
2021-08-30 22:38:10 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.4584ef61e2a67535c7266219c44329fe.js"
 );
 
 self.addEventListener('message', (event) => {
